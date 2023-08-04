04b33607877d6493e376bf626570e28833a7b29d
nursmdev <nsmaulidi12@gmail.com>
2023-08-04 15:08:12 +0700

    Update link-button.blade.php

--- resources/views/components/link-button.blade.php
+++ resources/views/components/link-button.blade.php
@@ -1,6 +1,6 @@
 <a {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-3 bg-indigo-600 border
-    border-transparent rounded-lg font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-900
-    focus:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2
+    rounded-lg font-semibold text-xs uppercase tracking-widest hover:bg-indigo-900
+    focus:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2
     transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </a>
